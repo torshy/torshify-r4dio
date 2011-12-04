99c94044bfbaff9e914a9e70c7b0c36949ff0621
Torstein Auensen <torshy@gmail.com>
2011-12-04 16:41:20 +0100

    Added research station to experiment with stuff.

---
+++ src/Torshify.Radio.EchoNest/Research/ResearchView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Practices.Prism.Regions;
+
+namespace Torshify.Radio.EchoNest.Research
+{
+    public partial class ResearchView : UserControl
+    {
+        private readonly IRegionManager _regionManager;
+
+        public ResearchView(IRegionManager regionManager)
+        {
+            InitializeComponent();
+            _regionManager = regionManager;
+            RegionManager.SetRegionManager(_contentControl, regionManager); 
+            _regionManager.RequestNavigate("ResearchRegion", new Uri("/ResearchViewChild2", UriKind.Relative));
+        }
+    }
+}
---
+++ src/Torshify.Radio.EchoNest/Research/ResearchViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Practices.Prism.ViewModel;
+
+namespace Torshify.Radio.EchoNest.Research
+{
+    public class ResearchViewModel : NotificationObject
+    {
+         
+    }
+}
