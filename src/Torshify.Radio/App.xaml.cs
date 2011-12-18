0a02d37e0d0f9ded96aea0a26073165611edaf57
Torstein Auensen <torshy@gmail.com>
2011-12-18 17:02:20 +0100

    Made it a single instance application

--- src/Torshify.Radio/App.xaml.cs
+++ src/Torshify.Radio/App.xaml.cs
@@ -1,9 +1,47 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+using Microsoft.Shell;
 
 namespace Torshify.Radio
 {
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
+        #region Fields
+
+        private const string Unique = "Torshify.R4dio";
+
+        #endregion Fields
+
+        #region Methods
+
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
+            {
+                var application = new App();
+
+                application.InitializeComponent();
+                application.Run();
+
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            if (MainWindow.WindowState == WindowState.Minimized)
+            {
+                MainWindow.WindowState = WindowState.Normal;
+            }
+
+            MainWindow.Activate();
+
+            return true;
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
@@ -11,5 +49,7 @@ namespace Torshify.Radio
             RadioBootstrapper bootstrapper = new RadioBootstrapper();
             bootstrapper.Run();
         }
+
+        #endregion Methods
     }
 }
