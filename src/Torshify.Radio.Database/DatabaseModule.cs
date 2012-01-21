952ee8544e5c5f70827e1b4703e9d9370492385c
Torstein Auensen <torshy@gmail.com>
2012-01-21 13:37:18 +0100

    Added empty database module

---
+++ src/Torshify.Radio.Database/DatabaseModule.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Practices.Prism.MefExtensions.Modularity;
+using Microsoft.Practices.Prism.Modularity;
+
+namespace Torshify.Radio.Database
+{
+    [ModuleExport("Database", typeof(DatabaseModule))]
+    public class DatabaseModule : IModule
+    {
+        public void Initialize()
+        {
+        }
+    }
+}
---
+++ src/Torshify.Radio.Database/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Torshify.Radio.Database")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("eb0447ed-61f5-48b7-b149-9284664cc2f9")]
