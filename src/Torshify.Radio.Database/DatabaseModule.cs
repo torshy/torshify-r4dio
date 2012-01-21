0fd4c9ddc03c8e738ea6f9d1c4b5290ece3b2b6e
Torstein Auensen <torshy@gmail.com>
2012-01-21 14:20:16 +0100

    Exported IDocumentStore from the database module

--- src/Torshify.Radio.Database/DatabaseModule.cs
+++ src/Torshify.Radio.Database/DatabaseModule.cs
@@ -1,8 +1,10 @@
-﻿using System.IO;
+﻿using System.ComponentModel.Composition;
+using System.IO;
 
 using Microsoft.Practices.Prism.MefExtensions.Modularity;
 using Microsoft.Practices.Prism.Modularity;
 
+using Raven.Client;
 using Raven.Client.Embedded;
 
 using Torshify.Radio.Framework;
@@ -12,16 +14,37 @@ namespace Torshify.Radio.Database
     [ModuleExport("Database", typeof(DatabaseModule))]
     public class DatabaseModule : IModule
     {
+        #region Fields
+
+        private EmbeddableDocumentStore _documentStore;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public DatabaseModule()
+        {
+            _documentStore = new EmbeddableDocumentStore();
+            _documentStore.DataDirectory = Path.Combine(AppConstants.AppDataFolder, "Database");
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        [Export]
+        public IDocumentStore DocumentStore
+        {
+            get { return _documentStore; }
+        }
+
+        #endregion Properties
+
         #region Methods
 
         public void Initialize()
         {
-            var documentStore = new EmbeddableDocumentStore
-            {
-                DataDirectory = Path.Combine(AppConstants.AppDataFolder, "Database")
-            };
-
-            documentStore.Initialize();
+            DocumentStore.Initialize();
         }
 
         #endregion Methods
---
+++ src/Torshify.Radio.Spotify/SpotifyModule.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Composition;
+
+using Microsoft.Practices.Prism.MefExtensions.Modularity;
+using Microsoft.Practices.Prism.Modularity;
+
+using Raven.Client;
+
+namespace Torshify.Radio.Spotify
+{
+    [ModuleExport(typeof(SpotifyModule), DependsOnModuleNames = new[] { "Database" })]
+    public class SpotifyModule : IModule
+    {
+        #region Properties
+
+        [Import]
+        public IDocumentStore DocumentStore
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        public void Initialize()
+        {
+
+        }
+
+        #endregion Methods
+    }
+}
