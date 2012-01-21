81388ec62201a4f25d0e387e72224e824004c03f
Torstein Auensen <torshy@gmail.com>
2012-01-21 23:28:16 +0100

    Styling TextBox and WatermarkTextBox

---
+++ src/Torshify.Radio.Core/Resources/R4dioIcon.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Torshify.Radio.Core.Resources
+{
+    public partial class R4dioIcon : Viewbox
+    {
+        public R4dioIcon()
+        {
+            InitializeComponent();
+        }
+    }
+}
