c66593f4ab26b6e3e3b49f54ed48f65133dd1efb
Torstein Auensen <torshy@gmail.com>
2012-01-21 13:51:24 +0100

    Added RavenDb

--- src/Torshify.Radio.Database/DatabaseModule.cs
+++ src/Torshify.Radio.Database/DatabaseModule.cs
@@ -1,13 +1,29 @@
-﻿using Microsoft.Practices.Prism.MefExtensions.Modularity;
+﻿using System.IO;
+
+using Microsoft.Practices.Prism.MefExtensions.Modularity;
 using Microsoft.Practices.Prism.Modularity;
+
+using Raven.Client.Embedded;
+
+using Torshify.Radio.Framework;
 
 namespace Torshify.Radio.Database
 {
     [ModuleExport("Database", typeof(DatabaseModule))]
     public class DatabaseModule : IModule
     {
+        #region Methods
+
         public void Initialize()
         {
+            var documentStore = new EmbeddableDocumentStore
+            {
+                DataDirectory = Path.Combine(AppConstants.AppDataFolder, "Database")
+            };
+
+            documentStore.Initialize();
         }
+
+        #endregion Methods
     }
 }
