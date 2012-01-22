9b43aaf1b6f75a4620d91b29724569efdb1457f5
Torstein Auensen <torshy@gmail.com>
2012-01-22 14:52:25 +0100

    Added back/forward navigation to view

--- src/Torshify.Radio.Core/Views/MainViewModel.cs
+++ src/Torshify.Radio.Core/Views/MainViewModel.cs
@@ -1,15 +1,47 @@
 ﻿using System.ComponentModel.Composition;
+
 using Microsoft.Practices.Prism.Regions;
 using Microsoft.Practices.Prism.ViewModel;
+using Torshify.Radio.Framework;
+using Torshify.Radio.Framework.Commands;
 
 namespace Torshify.Radio.Core.Views
 {
     [Export(typeof(MainViewModel))]
     public class MainViewModel : NotificationObject, INavigationAware
     {
-        void INavigationAware.OnNavigatedTo(NavigationContext navigationContext)
+        #region Constructors
+
+        public MainViewModel()
         {
+            NavigateBackCommand = new AutomaticCommand(ExecuteNavigateBack, CanExecuteNavigateBack);
+            NavigateForwardCommand = new AutomaticCommand(ExecuteNavigateForward, CanExecuteNavigateForward);
         }
+
+        #endregion Constructors
+
+        #region Properties
+
+        [Import]
+        public IRegionManager RegionManager
+        {
+            get; 
+            set;
+        }
+
+        public AutomaticCommand NavigateBackCommand
+        {
+            get; private set;
+        }
+
+        public AutomaticCommand NavigateForwardCommand
+        {
+            get; private set;
+        }
+
+        #endregion Properties
+
+        #region Methods
 
         bool INavigationAware.IsNavigationTarget(NavigationContext navigationContext)
         {
@@ -19,5 +51,31 @@ namespace Torshify.Radio.Core.Views
         void INavigationAware.OnNavigatedFrom(NavigationContext navigationContext)
         {
         }
+
+        void INavigationAware.OnNavigatedTo(NavigationContext navigationContext)
+        {
+        }
+
+        private bool CanExecuteNavigateBack()
+        {
+            return RegionManager.Regions[AppRegions.ViewRegion].NavigationService.Journal.CanGoBack;
+        }
+
+        private bool CanExecuteNavigateForward()
+        {
+            return RegionManager.Regions[AppRegions.ViewRegion].NavigationService.Journal.CanGoForward;
+        }
+
+        private void ExecuteNavigateBack()
+        {
+            RegionManager.Regions[AppRegions.ViewRegion].NavigationService.Journal.GoBack();
+        }
+
+        private void ExecuteNavigateForward()
+        {
+            RegionManager.Regions[AppRegions.ViewRegion].NavigationService.Journal.GoForward();
+        }
+
+        #endregion Methods
     }
 }
