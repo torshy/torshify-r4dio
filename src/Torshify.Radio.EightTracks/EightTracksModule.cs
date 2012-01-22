1127c5eccbbabd0b735395e569eefd8f63ba5cea
Torstein Auensen <torshy@gmail.com>
2012-01-22 17:19:40 +0100

    Added 8tracks module

---
+++ src/Torshify.Radio.EightTracks/EightTracksModule.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Practices.Prism.MefExtensions.Modularity;
+using Microsoft.Practices.Prism.Modularity;
+
+namespace Torshify.Radio.EightTracks
+{
+    [ModuleExport(typeof(EightTracksModule), DependsOnModuleNames = new[] { "Core" })]
+    public class EightTracksModule : IModule
+    {
+        public void Initialize()
+        {
+        }
+    }
+}
---
+++ src/Torshify.Radio.Grooveshark/GroovesharkModule.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Practices.Prism.MefExtensions.Modularity;
+using Microsoft.Practices.Prism.Modularity;
+
+namespace Torshify.Radio.Grooveshark
+{
+    [ModuleExport(typeof(GroovesharkModule), DependsOnModuleNames = new[] { "Core" })]
+    public class GroovesharkModule : IModule
+    {
+        public void Initialize()
+        {
+        }
+    }
+}
