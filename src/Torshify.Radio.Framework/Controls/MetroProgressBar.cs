3fa51fbf40fe145cd6e9971d039573fd4598a5f5
Torstein Auensen <torshy@gmail.com>
2012-01-23 21:34:18 +0100

    Added MetroProgressBar which looks like the wp7 progressbar

---
+++ src/Torshify.Radio.Framework/Controls/MetroProgressBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Torshify.Radio.Framework.Controls
+{
+    public class MetroProgressBar : ProgressBar
+    {
+         static MetroProgressBar()
+         {
+             DefaultStyleKeyProperty.OverrideMetadata(typeof(MetroProgressBar), new FrameworkPropertyMetadata(typeof(MetroProgressBar)));
+         }
+    }
+}
