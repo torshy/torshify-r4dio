df2ffac2d8dc72ce48da116a72a901e63cefa88c
Torstein Auensen <torshy@gmail.com>
2012-01-24 17:56:26 +0100

    Added IsSelected and IsSelectedAction to HeaderInfo

--- src/Torshify.Radio.Framework/HeaderInfo.cs
+++ src/Torshify.Radio.Framework/HeaderInfo.cs
@@ -1,9 +1,85 @@
+using System;
+
+using Microsoft.Practices.Prism.ViewModel;
+
 namespace Torshify.Radio.Framework
 {
-    public class HeaderInfo
+    public class HeaderInfo : NotificationObject
     {
-        public string ToolTip { get; set; }
-        public string IconUri { get; set; }
-        public string Title { get; set; }
+        #region Fields
+
+        private string _iconUri;
+        private bool _isSelected;
+        private string _title;
+        private object _toolTip;
+
+        #endregion Fields
+
+        #region Properties
+
+        public string IconUri
+        {
+            get { return _iconUri; }
+            set
+            {
+                if (_iconUri != value)
+                {
+                    _iconUri = value;
+                    RaisePropertyChanged("IconUri");
+                }
+            }
+        }
+
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    RaisePropertyChanged("IsSelected");
+
+                    if (IsSelectedAction != null)
+                    {
+                        IsSelectedAction(value);
+                    }
+                }
+            }
+        }
+
+        public Action<bool> IsSelectedAction
+        {
+            get;
+            set;
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (_title != value)
+                {
+                    _title = value;
+                    RaisePropertyChanged("Title");
+                }
+            }
+        }
+
+        public object ToolTip
+        {
+            get { return _toolTip; }
+            set
+            {
+                if (_toolTip != value)
+                {
+                    _toolTip = value;
+                    RaisePropertyChanged("ToolTip");
+                }
+            }
+        }
+
+        #endregion Properties
     }
 }
--- src/Torshify.Radio/Utilities/SingleInstance.cs
+++ src/Torshify.Radio/Utilities/SingleInstance.cs
@@ -26,6 +26,60 @@ using System.Windows.Threading;
 //-----------------------------------------------------------------------
 namespace Torshify.Radio.Utilities
 {
+    [SuppressUnmanagedCodeSecurity]
+    internal static class NativeMethods
+    {
+        #region Delegates
+
+        /// <summary>
+        /// Delegate declaration that matches WndProc signatures.
+        /// </summary>
+        public delegate IntPtr MessageHandler(WM uMsg, IntPtr wParam, IntPtr lParam, out bool handled);
+
+        #endregion Delegates
+
+        #region Methods
+
+        public static string[] CommandLineToArgvW(string cmdLine)
+        {
+            IntPtr argv = IntPtr.Zero;
+            try
+            {
+                int numArgs = 0;
+
+                argv = _CommandLineToArgvW(cmdLine, out numArgs);
+                if (argv == IntPtr.Zero)
+                {
+                    throw new Win32Exception();
+                }
+                var result = new string[numArgs];
+
+                for (int i = 0; i < numArgs; i++)
+                {
+                    IntPtr currArg = Marshal.ReadIntPtr(argv, i * Marshal.SizeOf(typeof(IntPtr)));
+                    result[i] = Marshal.PtrToStringUni(currArg);
+                }
+
+                return result;
+            }
+            finally
+            {
+                IntPtr p = _LocalFree(argv);
+
+                // Otherwise LocalFree failed.
+                // Assert.AreEqual(IntPtr.Zero, p);
+            }
+        }
+
+        [DllImport("shell32.dll", EntryPoint = "CommandLineToArgvW", CharSet = CharSet.Unicode)]
+        private static extern IntPtr _CommandLineToArgvW([MarshalAs(UnmanagedType.LPWStr)] string cmdLine, out int numArgs);
+
+        [DllImport("kernel32.dll", EntryPoint = "LocalFree", SetLastError = true)]
+        private static extern IntPtr _LocalFree(IntPtr hMem);
+
+        #endregion Methods
+    }
+
     #region Enumerations
 
     internal enum WM
@@ -403,60 +457,6 @@ namespace Torshify.Radio.Utilities
         #endregion Methods
 
         #region Nested Types
-
-        [SuppressUnmanagedCodeSecurity]
-        internal static class NativeMethods
-        {
-            #region Delegates
-
-            /// <summary>
-            /// Delegate declaration that matches WndProc signatures.
-            /// </summary>
-            public delegate IntPtr MessageHandler(WM uMsg, IntPtr wParam, IntPtr lParam, out bool handled);
-
-            #endregion Delegates
-
-            #region Methods
-
-            public static string[] CommandLineToArgvW(string cmdLine)
-            {
-                IntPtr argv = IntPtr.Zero;
-                try
-                {
-                    int numArgs = 0;
-
-                    argv = _CommandLineToArgvW(cmdLine, out numArgs);
-                    if (argv == IntPtr.Zero)
-                    {
-                        throw new Win32Exception();
-                    }
-                    var result = new string[numArgs];
-
-                    for (int i = 0; i < numArgs; i++)
-                    {
-                        IntPtr currArg = Marshal.ReadIntPtr(argv, i * Marshal.SizeOf(typeof(IntPtr)));
-                        result[i] = Marshal.PtrToStringUni(currArg);
-                    }
-
-                    return result;
-                }
-                finally
-                {
-                    IntPtr p = _LocalFree(argv);
-
-                    // Otherwise LocalFree failed.
-                    // Assert.AreEqual(IntPtr.Zero, p);
-                }
-            }
-
-            [DllImport("shell32.dll", EntryPoint = "CommandLineToArgvW", CharSet = CharSet.Unicode)]
-            private static extern IntPtr _CommandLineToArgvW([MarshalAs(UnmanagedType.LPWStr)] string cmdLine, out int numArgs);
-
-            [DllImport("kernel32.dll", EntryPoint = "LocalFree", SetLastError = true)]
-            private static extern IntPtr _LocalFree(IntPtr hMem);
-
-            #endregion Methods
-        }
 
         /// <summary>
         /// Remoting service class which is exposed by the server i.e the first instance and called by the second instance
