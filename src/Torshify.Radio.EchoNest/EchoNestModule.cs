cdbfe0f31c26cb25fdc9d2c986e1d8efc546b4e3
Torstein Auensen <torshy@gmail.com>
2012-01-25 20:56:51 +0100

    Added EchoNest project. Removed unused references

---
+++ src/Torshify.Radio.EchoNest/EchoNestModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Practices.Prism.MefExtensions.Modularity;
+using Microsoft.Practices.Prism.Modularity;
+
+namespace Torshify.Radio.EchoNest
+{
+    [ModuleExport(typeof(EchoNestModule), DependsOnModuleNames = new[] { "Core" })]
+    public class EchoNestModule : IModule
+    {
+        public void Initialize()
+        {
+
+        }
+    }
+}
