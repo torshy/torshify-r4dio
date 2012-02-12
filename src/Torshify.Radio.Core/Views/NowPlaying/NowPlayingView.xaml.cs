373a960500cbf79766a079cfb44138a778e16c46
Torstein Auensen <torshy@gmail.com>
2012-02-12 19:20:14 +0100

    Suppressed XButton2 while being in NowPlaying view to stop the user from navigating without knowing it

--- src/Torshify.Radio.Core/Views/NowPlaying/NowPlayingView.xaml.cs
+++ src/Torshify.Radio.Core/Views/NowPlaying/NowPlayingView.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System.ComponentModel.Composition;
 using System.Windows.Controls;
+using System.Windows.Input;
+
 using Microsoft.Practices.Prism.Regions;
+
+using Torshify.Radio.Framework.Input;
 
 namespace Torshify.Radio.Core.Views.NowPlaying
 {
@@ -9,16 +13,48 @@ namespace Torshify.Radio.Core.Views.NowPlaying
     [RegionMemberLifetime(KeepAlive = false)]
     public partial class NowPlayingView : UserControl
     {
+        #region Constructors
+
         public NowPlayingView()
         {
             InitializeComponent();
         }
 
+        #endregion Constructors
+
+        #region Properties
+
         [Import]
         public NowPlayingViewModel Model
         {
             get { return DataContext as NowPlayingViewModel; }
-            set { DataContext = value; }
+            set
+            {
+                DataContext = value;
+                InputBindings.Add(new ExtendedMouseBinding
+                {
+                    Command = value.NavigateBackCommand,
+                    Gesture = new ExtendedMouseGesture(MouseButton.XButton1)
+                });
+            }
         }
+
+        #endregion Properties
+
+        #region Methods
+
+        protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
+        {
+            // Prevent the NavigateForward command to execute in the background while displaying the NowPlaying view.
+            if (e.ChangedButton == MouseButton.XButton2)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            base.OnPreviewMouseDown(e);
+        }
+
+        #endregion Methods
     }
 }
