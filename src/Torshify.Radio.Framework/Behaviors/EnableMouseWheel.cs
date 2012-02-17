1ec8fa2e75747679f988ab53006b22c428173aa8
Torstein Auensen <torshy@gmail.com>
2012-02-17 22:00:10 +0100

    Added slider mouse-wheel support

---
+++ src/Torshify.Radio.Framework/Behaviors/EnableMouseWheel.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace Torshify.Radio.Framework.Behaviors
+{
+    [System.ComponentModel.Description("Target : Trigger. Responds to mousewheel event of attached object when focused. Manipulates slider values based on slider properties.")]
+    public class EnableMouseWheel : TargetedTriggerAction<Slider>
+    {
+        #region Methods
+
+        //this method is required for the base class
+        protected override void Invoke(object parameter)
+        {
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            //add event handler to mouse wheel event
+            ((FrameworkElement)AssociatedObject).MouseWheel += SliderMouseScrollMouseWheel;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            //remove the event handler
+            ((FrameworkElement)AssociatedObject).MouseWheel -= SliderMouseScrollMouseWheel;
+        }
+
+        //this method runs on Mouse Wheel event
+        private void SliderMouseScrollMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            //Delta = how much the wheel was scrolled. + for up - for down
+
+            //check to see if Delta is positive.
+            if (e.Delta > 0)
+            {
+                //using small change of Target(slider) property.
+                //this allows how much value can change from the designer
+                //
+                //alternatively you can actually use the delta value instead
+                //of forcing small change increments.
+
+                Target.Value = Target.Value + Target.SmallChange;
+            }
+            else //same as above just backwards
+            {
+                //using small change of Target(slider) property.
+                //this allows how much value can change from the designer
+                //
+                //alternatively you can actually use the delta value instead
+                //of forcing small change increments.
+
+                Target.Value = Target.Value - Target.SmallChange;
+            }
+        }
+
+        #endregion Methods
+    }
+}
---
+++ src/Torshify.Radio.Framework/Behaviors/EnableSliderMouseWheel.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace Torshify.Radio.Framework.Behaviors
+{
+    public class EnableSliderMouseWheel : Behavior<Slider>
+    {
+        #region Methods
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            //attach event handler on mouse wheel event
+            AssociatedObject.MouseWheel += SliderMouseScrollMouseWheel;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            //remove event handler on mouse wheel event
+            AssociatedObject.MouseWheel -= SliderMouseScrollMouseWheel;
+        }
+
+        private void SliderMouseScrollMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            //Delta = how much the wheel was scrolled. + for up - for down
+
+            //check to see if Delta is positive.
+            if (e.Delta > 0)
+            {
+                //using small change of Target(slider) property.
+                //this allows how much value can change from the designer
+                //
+                //alternatively you can actually use the delta value instead
+                //of forcing small change increments.
+
+                AssociatedObject.Value = AssociatedObject.Value + AssociatedObject.SmallChange;
+            }
+            else //same as above just backwards
+            {
+                //using small change of Target(slider) property.
+                //this allows how much value can change from the designer
+                //
+                //alternatively you can actually use the delta value instead
+                //of forcing small change increments.
+
+                AssociatedObject.Value = AssociatedObject.Value - AssociatedObject.SmallChange;
+            }
+        }
+
+        #endregion Methods
+    }
+}
