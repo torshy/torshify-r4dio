a295173541d05c9c0b22ad268ed62d9d5bd0048f
Torstein Auensen <torshy@gmail.com>
2012-02-21 22:09:16 +0100

    Added sparkle to Shell

--- src/Torshify.Radio/Shell.xaml.cs
+++ src/Torshify.Radio/Shell.xaml.cs
@@ -1,14 +1,31 @@
 ﻿using System.ComponentModel.Composition;
+
+using AppLimit.NetSparkle;
+
 using Torshify.Radio.Framework.Controls;
+using Torshify.Radio.Properties;
 
 namespace Torshify.Radio
 {
     [Export(typeof(Shell))]
     public partial class Shell : MetroWindow
     {
+        #region Fields
+
+        private Sparkle _sparkle;
+
+        #endregion Fields
+
+        #region Constructors
+
         public Shell()
         {
             InitializeComponent();
+
+            _sparkle = new Sparkle(Settings.Default.VersionInfoUri);
+            _sparkle.StartLoop(true, true);
         }
+
+        #endregion Constructors
     }
 }
