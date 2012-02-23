8bda559bb2b32068f8936087073a8711ff520e20
Torstein Auensen <torshy@gmail.com>
2012-02-23 09:03:50 +0100

    Increased version

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -3,9 +3,9 @@
 [assembly: AssemblyCompany("Torstein Auensen")]
 [assembly: AssemblyProduct("r4dio")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
-[assembly: AssemblyInformationalVersion("0.5.0.0")]
+[assembly: AssemblyVersion("0.5.1.0")]
+[assembly: AssemblyFileVersion("0.5.1.0")]
+[assembly: AssemblyInformationalVersion("0.5.1.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
