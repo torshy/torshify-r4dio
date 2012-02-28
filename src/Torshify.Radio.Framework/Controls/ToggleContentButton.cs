8caa175fed7c013f0a47657eab5013714937d31f
Torstein Auensen <torshy@gmail.com>
2012-02-28 17:42:02 +0100

    Added ToggleContentButton to have a simple togglebutton that changes apperance

---
+++ src/Torshify.Radio.Framework/Controls/ToggleContentButton.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Torshify.Radio.Framework.Controls
+{
+    public class ToggleContentButton : ToggleButton
+    {
+        #region Fields
+
+        public static readonly DependencyProperty CheckedContentProperty = 
+            DependencyProperty.Register("CheckedContent", typeof(object), typeof(ToggleContentButton),
+                new FrameworkPropertyMetadata((object)null));
+        public static readonly DependencyProperty NonCheckedContentProperty = 
+            DependencyProperty.Register("NonCheckedContent", typeof(object), typeof(ToggleContentButton),
+                new FrameworkPropertyMetadata((object)null));
+
+        #endregion Fields
+
+        #region Constructors
+
+        static ToggleContentButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ToggleContentButton), new FrameworkPropertyMetadata(typeof(ToggleContentButton)));
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public object NonCheckedContent
+        {
+            get
+            {
+                return (object)GetValue(NonCheckedContentProperty);
+            }
+            set
+            {
+                SetValue(NonCheckedContentProperty, value);
+            }
+        }
+
+        public object CheckedContent
+        {
+            get
+            {
+                return (object)GetValue(CheckedContentProperty);
+            }
+            set
+            {
+                SetValue(CheckedContentProperty, value);
+            }
+        }
+
+        #endregion Properties
+    }
+}
