0d70e00c48ce211fa0e2db0e7f82e106326e2ab3
Torstein Auensen <torshy@gmail.com>
2012-03-03 11:43:16 +0100

    Added automatic scroll to top when list resets. fixes #3

--- src/Torshify.Radio.EightTracks/Views/Tabs/MixListView.xaml.cs
+++ src/Torshify.Radio.EightTracks/Views/Tabs/MixListView.xaml.cs
@@ -1,12 +1,32 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Specialized;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace Torshify.Radio.EightTracks.Views.Tabs
 {
     public partial class MixListView : UserControl
     {
+        #region Constructors
+
         public MixListView()
         {
             InitializeComponent();
+
+            MixList.ItemContainerGenerator.ItemsChanged += ItemContainerGeneratorOnItemsChanged;
         }
+
+        #endregion Constructors
+
+        #region Methods
+
+        private void ItemContainerGeneratorOnItemsChanged(object sender, ItemsChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Reset)
+            {
+                MixListScrollViewer.ScrollToTop();
+            }
+        }
+
+        #endregion Methods
     }
 }
