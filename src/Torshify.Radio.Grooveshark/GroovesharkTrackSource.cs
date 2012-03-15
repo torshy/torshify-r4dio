e82e8951d187b3ca216b47b9dfb4ff494e776a07
Torstein Auensen <torshy@gmail.com>
2012-03-15 08:16:48 +0100

    Updated Grooveshark libs

--- src/Torshify.Radio.Grooveshark/GroovesharkTrackSource.cs
+++ src/Torshify.Radio.Grooveshark/GroovesharkTrackSource.cs
@@ -132,13 +132,21 @@ namespace Torshify.Radio.Grooveshark
 
         private IEnumerable<Track> Convert(IEnumerable<SearchArtist.SearchArtistResult> songs)
         {
-            return songs.Select(result => new GroovesharkTrack(result.SongID, result.AlbumID)
+            return songs.Select(result =>
             {
-                Name = result.SongName,
-                Album = result.AlbumName,
-                Artist = result.ArtistName,
-                Index = TryParse(result.TrackNum),
-                AlbumArt = "http://images.grooveshark.com/static/albums/90_" + result.AlbumID + ".jpg"
+                int songId;
+                int albumId;
+                int.TryParse(result.SongID, out songId);
+                int.TryParse(result.AlbumID, out albumId);
+
+                return new GroovesharkTrack(songId, albumId)
+                {
+                    Name = result.SongName,
+                    Album = result.AlbumName,
+                    Artist = result.ArtistName,
+                    Index = TryParse(result.TrackNum),
+                    AlbumArt = "http://images.grooveshark.com/static/albums/90_" + result.AlbumID + ".jpg"
+                };
             });
         }
 
