138af52297dfe733df2b2a77a5d314e3fe22daac
Torstein Auensen <torshy@gmail.com>
2012-03-25 11:19:15 +0200

    Added some icons in the eclectic view

---
+++ src/Torshify.Radio.Framework/Controls/MetroButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Torshify.Radio.Framework.Controls
+{
+    public class MetroButton : Button
+    {
+        #region Fields
+
+        public static readonly DependencyProperty MetroImageSourceProperty = 
+            DependencyProperty.Register(
+                "MetroImageSource", 
+                typeof(Visual), 
+                typeof(MetroButton), 
+                new PropertyMetadata(default(Visual)));
+
+        #endregion Fields
+
+        #region Constructors
+
+        public MetroButton()
+        {
+            DefaultStyleKey = typeof(MetroButton);
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public Visual MetroImageSource
+        {
+            get
+            {
+                return (Visual)GetValue(MetroImageSourceProperty);
+            }
+            set
+            {
+                SetValue(MetroImageSourceProperty, value);
+            }
+        }
+
+        #endregion Properties
+    }
+}
